c5d84e6d721cc132453f097170e748dff69418cc
XieJun <1019163135@qq.com>
2020-03-31 15:38:30 +0800

    first commit

---
+++ app/src/main/java/dhht/android/datatranslate/MainActivity.java
@@ -0,0 +1,16 @@
+package dhht.android.datatranslate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import dhht.android.datatranslatelibrary.DataUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
